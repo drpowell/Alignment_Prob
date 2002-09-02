a4061c91613020a8975e8f60ae9461de458f262e
David Powell <david@drp.id.au>
2002-09-02 00:35:21 +0000

    Plot raw SW scores from PRSS

--- alignCompress/testRuns/populationPlot.pl
+++ alignCompress/testRuns/populationPlot.pl
@@ -6,20 +6,24 @@ my $prss;
 
 if (defined($ARGV[0]) && (
     $ARGV[0] eq 'prss' ||
+    $ARGV[0] eq 'prss2' ||
     $ARGV[0] eq 'al_all' ||
     $ARGV[0] eq 'al_one')) {
     $prss = shift;
 }
 
-(defined($prss)) or die "Usage: $0 <prss|al_all|al_one>";
+(defined($prss)) or die "Usage: $0 <prss|prss2|al_all|al_one>";
+# prss  - plot using p value
+# prss2 - plot using raw score
 
-my(@prss, @al_all, @al_one);
+my(@prss, @prss2, @al_all, @al_one);
 
 while (<>) {
   next if (/^\#/);
 
-  if (/^PRSS: s1=(\d+) s2=(\d+) parent=(\d+) mutates=(\d+) p=(\S+)/) {
+  if (/^PRSS: s1=(\d+) s2=(\d+) parent=(\d+) mutates=(\d+) p=(\S+) s=(\d+)/) {
     push(@prss,{S1=>$1, S2=>$2, PARENT=>$3, MUTATES=>$4, VAL=>(1-$5)});
+    push(@prss2,{S1=>$1, S2=>$2, PARENT=>$3, MUTATES=>$4, VAL=>($6)});
     next;
   }
 
@@ -37,6 +41,7 @@ while (<>) {
 my $arr;
 
 ($prss eq 'prss')   && ($arr = \@prss);
+($prss eq 'prss2')   && ($arr = \@prss2);
 ($prss eq 'al_all') && ($arr = \@al_all);
 ($prss eq 'al_one') && ($arr = \@al_one);
 
