3dc794a961156b26063598936bba32dc267f1f0d
David Powell <david@drp.id.au>
2005-09-06 02:32:53 +0000

    Updated version to v1.3

--- fuzzyLZ/FuzzyDriver.java
+++ fuzzyLZ/FuzzyDriver.java
@@ -21,7 +21,7 @@ import common.*;
 
 public class FuzzyDriver implements Serializable {
     
-    static final String VERSION = "1.2";
+    static final String VERSION = "1.3";
     
     private static final double CONVERGE_CUTOFF = 0.0001;
     
