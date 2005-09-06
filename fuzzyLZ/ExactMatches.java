4ad62484ef6ee257e75cc9c85b209769bc974a70
David Powell <david@drp.id.au>
2005-09-06 02:30:23 +0000

    Formatting modification.  Split Convert.conv() into two methods.  Removed the unused Reverse_Convert class.

--- fuzzyLZ/ExactMatches.java
+++ fuzzyLZ/ExactMatches.java
@@ -1,8 +1,8 @@
-/*  
+/*
  *  Copyright (c) David Powell <david@drp.id.au>
  *
- * 
- * This file is part of FuzzyLZ 
+ *
+ * This file is part of FuzzyLZ
  *
  * FuzzyLZ is a program orginally intended for the
  * compression of DNA sequeces.  It can be viewed as a
@@ -23,40 +23,38 @@ import java.io.Serializable;
 /**
  * This class implements a hashing scheme to find matches of a fixed size within
  * a String
- * 
+ *
  * @author David Powell
  * @version 24/5/2001
  */
 
 public class ExactMatches implements Serializable {
-
+    
     //    public static class Convert implements Serializable {
     public static class Convert implements Serializable {
         public String conv(String s) {
             return s;
         }
+        
+        public char conv(char a) {
+            return a;
+        }
     }
-
-    public static class Reverse_Convert extends Convert {
-        public String conv(String s) {
-            //return (new StringBuffer(s)).reverse().toString();
-            char c[] = s.toCharArray();
-            for (int i = 0; i < c.length / 2; i++) {
-                char a = c[i];
-                c[i] = c[c.length - i - 1];
-                c[c.length - i - 1] = a;
-            }
-            return new String(c);
-        };
-    }
-
+    
     public static class Reverse_Complement_DNA extends Convert {
         public String conv(String s) {
             //return (new StringBuffer(s)).reverse().toString();
             char c[] = new char[s.length()];
             for (int i = 0; i < s.length(); i++) {
                 char a = s.charAt(i);
-                switch (a) {
+                a = conv(a);
+                c[s.length() - i - 1] = a;
+            }
+            return new String(c);
+        };
+        
+        public char conv(char a) {
+            switch (a) {
                 case 'A':
                     a = 'T';
                     break;
@@ -84,61 +82,60 @@ public class ExactMatches implements Serializable {
                 default:
                     System.err.println("WARNING: unknown DNA character " + a
                             + " in Reverse_Complement_DNA (char unchanged)");
-                }
-                c[s.length() - i - 1] = a;
+                    return a;
             }
-            return new String(c);
-        };
+            return a;
+        }
     }
-
+    
     // Internal representation of a linked-list of ints.
     public static class MyList implements Serializable {
         public static class L implements Serializable {
             L next;
-
+            
             int val;
-
+            
             L(int v) {
                 next = null;
                 val = v;
             }
         }
-
+        
         L start, end;
-
+        
         MyList(int v) {
             L n = new L(v);
             start = end = n;
         }
-
+        
         void add(int v) {
             L n = new L(v);
             end.next = n;
             end = n;
         }
     }
-
+    
     private static class MyHash implements Serializable {
         private static class HashChain implements Serializable {
             HashChain next;
-
+            
             MyList intList;
         }
-
+        
         int hSize = 1048573;
-
+        
         HashChain hTable[];
-
+        
         char[] str;
-
+        
         int winSize;
-
+        
         MyHash(char[] str, int winSize) {
             this.str = str;
             this.winSize = winSize;
             hTable = new HashChain[hSize];
         }
-
+        
         private int hash(char[] s, int p) {
             int res = 0;
             for (int i = 0; i < winSize; i++) {
@@ -146,18 +143,18 @@ public class ExactMatches implements Serializable {
             }
             return Math.abs(res) % hSize;
         }
-
+        
         private boolean equal(char[] s1, int p1, char[] s2, int p2) {
             for (int i = 0; i < winSize; i++)
                 if (s1[p1 + i] != s2[p2 + i])
                     return false;
             return true;
         }
-
+        
         MyList get(String s) {
             return get(s.toCharArray(), 0);
         }
-
+        
         MyList get(char[] s, int ipos) {
             int i = hash(s, ipos);
             HashChain c = hTable[i];
@@ -165,7 +162,7 @@ public class ExactMatches implements Serializable {
             // s
             while (c != null) {
                 int pos = c.intList.start.val; // Get the string pos of the
-                                               // first.
+                // first.
                 if (equal(s, ipos, str, pos)) {
                     // A match!
                     return c.intList;
@@ -174,50 +171,50 @@ public class ExactMatches implements Serializable {
             }
             return null;
         }
-
+        
         void put(int pos) {
             MyList l = get(str, pos);
             if (l == null) {
                 // First occurance of this string
                 HashChain c = new HashChain();
                 c.intList = new MyList(pos);
-
+                
                 int i = hash(str, pos);
                 c.next = hTable[i];
                 hTable[i] = c;
             } else {
                 l.add(pos);
             }
-
+            
         }
     }
-
+    
     char[] str;
-
+    
     int winSize;
-
+    
     int strLen;
-
+    
     MyHash h;
-
+    
     ExactMatches(char[] str, int winSize) {
         this.str = str;
         this.winSize = winSize;
-
+        
         strLen = str.length;
-
+        
         if (FuzzyLZ.DEBUG >= 2)
             System.err.println("Constructing table of repeats...");
-
+        
         h = new MyHash(str, winSize);
         for (int i = 0; i < strLen + 1 - winSize; i++) {
             h.put(i);
         }
-
+        
         if (FuzzyLZ.DEBUG >= 2)
             System.err.println("Done constructing table of repeats.");
     }
-
+    
     // Write our own serization handler. We will _not_ save the hash table.
     // Only the string, and recompute the hash table on reload
     private void writeObject(java.io.ObjectOutputStream out) throws IOException {
@@ -225,33 +222,33 @@ public class ExactMatches implements Serializable {
         out.writeInt(winSize);
         out.writeInt(strLen);
     }
-
+    
     private void readObject(java.io.ObjectInputStream in) throws IOException,
             ClassNotFoundException {
         str = (char[]) in.readObject();
         winSize = in.readInt();
         strLen = in.readInt();
-
+        
         if (FuzzyLZ.DEBUG >= 2)
             System.err.println("Re-Constructing table of repeats...");
-
+        
         h = new MyHash(str, winSize);
         for (int i = 0; i < strLen + 1 - winSize; i++) {
             h.put(i);
         }
-
+        
         if (FuzzyLZ.DEBUG >= 2)
             System.err.println("Done re-constructing table of repeats.");
     }
-
+    
     MyList get(char[] s, int pos) {
         return h.get(s, pos);
     }
-
+    
     MyList get(String s) {
         return h.get(s);
     }
-
+    
     public long count_hits(Convert convert) {
         long count = 0;
         for (int i = 0; i < strLen + 1 - winSize; i++) {
@@ -265,7 +262,7 @@ public class ExactMatches implements Serializable {
         }
         return count;
     }
-
+    
     public void dispAll(Convert convert) {
         for (int i = 0; i < strLen + 1 - winSize; i++) {
             String s = new String(str, i, winSize);
@@ -280,11 +277,11 @@ public class ExactMatches implements Serializable {
             }
         }
     }
-
+    
     public static void main(String args[]) {
         int len = Integer.valueOf(args[0]).intValue();
         char[] input = null;
-
+        
         try {
             StringBuffer s = new StringBuffer();
             byte[] buf = new byte[1024];
@@ -297,12 +294,12 @@ public class ExactMatches implements Serializable {
             System.err.println("Unable to read stdin");
             System.exit(1);
         }
-
+        
         System.out.println("String length = " + input.length);
-
+        
         ExactMatches m = new ExactMatches(input, len);
         //	System.out.println("Number of matches = "+m.count());
-
+        
         if (true) {
             //m.dispAll(new Convert());
             m.dispAll(new Reverse_Complement_DNA());
